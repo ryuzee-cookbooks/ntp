38baea167af2e6bf08ed8a9f4b517a0f95c1862a
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:33:14 +0900

    clarify license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "ntp"
 maintainer       "ryuzee"
 maintainer_email "ryuzee@gmail.com"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures ntp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
