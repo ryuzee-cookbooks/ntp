a60569250ae546330a6757d9b9a99efcff25c24f
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-12-30 10:10:36 +0900

    fix foodcritic

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,36 +1,36 @@
 #
-# Cookbook Name:: ntp 
-# Recipe:: default 
+# Cookbook Name:: ntp
+# Recipe:: default
 #
 # Author:: Ryuzee <ryuzee@gmail.com>
 #
-# Copyright 2012, Ryutaro YOSHIBA 
+# Copyright 2012, Ryutaro YOSHIBA
 #
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
-case node[:platform]
+case node['platform']
 when 'centos', 'amazon'
 
   node['platform_version'].to_f >= 6.0 ? p = 'cronie' : p = 'vixie-cron'
 
   yum_package p do
-    action :install
+    action :upgrade
     flush_cache [:before]
   end
 
   yum_package 'ntp' do
-    action :install
+    action :upgrade
     flush_cache [:before]
   end
 
 when 'ubuntu', 'debian'
   include_recipe "apt"
   package 'cron' do
-    action :install
+    action :upgrade
   end
   package 'ntpdate' do
-    action :install
+    action :upgrade
   end
 end
 
@@ -49,7 +49,7 @@ end
 
 execute cmd do
   action :run
-  not_if {'ps -ef | grep ntpd'}
+  not_if 'ps -ef | grep ntpd'
 end
 
 # vim: filetype=ruby.chef
